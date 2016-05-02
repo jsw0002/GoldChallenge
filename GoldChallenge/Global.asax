0332ea57d2d27ebb607f00f6ada42284e073c6d4
Jordan Willis <jsw0002@gmail.com>
2016-05-02 13:23:38 -0400

    Add project files.

---
+++ GoldChallenge/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoldChallenge.MvcApplication" Language="C#" %>
---
+++ GoldChallenge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoldChallenge.Startup))]
+namespace GoldChallenge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
